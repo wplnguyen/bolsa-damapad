a1d9b7a56edb79bfa995cf319bce246a22bbaa14
wplnguyen <51763653+wplnguyen@users.noreply.github.com>
2022-07-18 01:39:26 -0700

    Update keymap.c

--- damapad/keymaps/vial/keymap.c
+++ damapad/keymaps/vial/keymap.c
@@ -20,20 +20,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_wkl(
-        QK_BOOT, KC_MPLY, KC_MNXT, KC_MSTP, KC_MSEL, KC_MUTE,
+        KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, KC_MSEL, KC_MUTE,
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_PGUP,  
         KC_NO,   KC_COPY,          KC_PASTE,         KC_PGDN
     )
 
 };
 
-/*
+
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [1] =  { ENCODER_CCW_CW(RGB_HUD, RGB_HUI)},
-    [2] =  { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
-    [3] = { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD)}
+    [0] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)}
 };
 #endif
-*/
